e82a755a776f2cde84de4337bd670480d6f04a06
Byung-yun <quddbstmvls@gmail.com>
2025-04-10 15:07:23 +0900

    update 음료수 클리어

--- 음료수_얼려_먹기.py
+++ 음료수_얼려_먹기.py
@@ -1,6 +1,10 @@
-# 테스트 케이스
+# 테스트 케이스 1
+# N, M = 4, 5
+# array = [[0, 0, 1, 1, 0], [0, 0, 0, 1, 1], [1, 1, 1, 1, 1], [0, 0, 0, 0, 0]]
+
+# 테스트 케이스 2
 N, M = 5, 5
-array = ["11011","10100","10100","00000","01011"]
+array = [[0, 0, 0, 0, 1], [0, 0, 0, 1, 0], [0, 0, 1, 0, 0], [0, 1, 0, 0, 0], [1, 0, 0, 0, 0]]
 
 # 입력
 # N, M = list(map(int, input().split()))
@@ -8,16 +12,46 @@ array = ["11011","10100","10100","00000","01011"]
 # for i in range(N):
 #     array.append(input())
 
-count = 0  # 얼음의 총 개수
+def mark(r,c):
+    if r < 0 or r >= N or c < 0 or c >= M:    # 맵 밖으로 나가면 무시
+        return False
+    if array[r][c] == 1:                      # 얼음이 아니면 무시
+        return False
+    array[r][c] = 1                           # 벽으로 채우기
 
-# 계산
-for row in array:
-    ices = []  # 얼음 위치를 기억할 리스트
-    for idx in range(len(row)):
-        if row[idx] == "1":
-            ices.append(idx)
-    print(ices)
+    # 주위를 벽으로 채우기
+    mark(r+1, c)
+    mark(r, c+1)
+    mark(r-1, c)
+    mark(r, c-1)
+
+    return True
 
 
+def debug_array():
+    for row in range(len(array)):
+        for col in range(len(array[row])):
+            if array[row][col] == 1:
+                print("□", end="")
+            else:
+                print('■', end="")
+        print()
+
+count = 0
 
 
+# 초기 상태
+print("초기 상태")
+debug_array()
+
+
+for row in range(len(array)):
+    for col in range(len(array[row])):
+        flag = mark(row, col)
+        if flag:
+            count += 1
+            print("-----", count, "-----")
+            debug_array()
+
+
+print(count)
