a3a434290ebd4b0abe53fc3365f99f3705aedd16
Byung-yun <quddbstmvls@gmail.com>
2025-04-10 16:18:29 +0900

    add 미로 탈출 추가

---
+++ 미로_탈출.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+N, M = 5, 6  # 탈출구
+array = [[1, 0, 1, 0, 1, 0],
+         [1, 1, 1, 1, 1, 1],
+         [0, 0, 0, 0, 0, 1],
+         [1, 1, 1, 1, 1, 1],
+         [1, 1, 1, 1, 1, 1]]
+
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    while queue:
+        x, y = queue.popleft()
+
+
